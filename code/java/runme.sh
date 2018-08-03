fa456a08151dc6a490e2fffa69e61218cf0962f9
Richard Quintin <rquintin@vt.edu>
2018-08-03 15:29:26 -0400

    expose port

--- code/java/runme.sh
+++ code/java/runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t app -f ./Dockerfile ../.. && \
-docker run -it --rm -v $(pwd)/maven:/maven --link postgres:postgres app $*
+docker run -it --rm -p 8080:8080 -v $(pwd)/maven:/maven --link postgres:postgres app $*
