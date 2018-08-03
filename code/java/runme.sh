bc7119105a2467a169f352dfaf763f0fd6321aee
Richard Quintin <rquintin@vt.edu>
2018-08-03 16:50:51 -0400

    map in code so we can test updates live

--- code/java/runme.sh
+++ code/java/runme.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 docker build -t app -f ./Dockerfile ../.. && \
-docker run -it --rm -p 8080:8080 -v $(pwd)/maven:/maven --link postgres:postgres app $*
+docker run -it --rm -p 8080:8080 \
+  -v $(pwd)/maven:/maven \
+  -v $(pwd)/troop:/code \
+  --link postgres:postgres \
+  app $*
