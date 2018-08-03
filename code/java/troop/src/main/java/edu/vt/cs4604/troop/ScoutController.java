8b69c9b59dbf9ac938d9e78e3221ac42bef82ab1
Richard Quintin <rquintin@vt.edu>
2018-08-03 16:51:19 -0400

    interim commit - can now load list of scouts on startup

--- code/java/troop/src/main/java/edu/vt/cs4604/troop/ScoutController.java
+++ code/java/troop/src/main/java/edu/vt/cs4604/troop/ScoutController.java
@@ -1,6 +1,8 @@
 package edu.vt.cs4604.troop;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
--- code/java/troop/static/js/main.js
+++ code/java/troop/static/js/main.js
@@ -1,7 +1,40 @@
+/*
 $(document).ready(function() {
-    $.ajax({
-        url: "/scouts/Timmy"
-    }).then(function(data) {
-       $('.greeting-id').append(data.rank);
-    });
+$.ajax({
+  url: "/scouts/Timmy"
+}).then(function(data) {
+   $('.greeting-id').append(data.rank);
+});
+});
+*/
+
+$(document).ready(function () {
+  $('#ScoutTableContainer').jtable({
+    title: 'Scouts',
+    fields: {
+      name: {
+        title: 'Name'
+      },
+      rank: {
+        title: 'Rank'
+      },
+    },
+    actions: {
+      listAction: function (postData, jtParams) {
+        return $.Deferred(function ($dfd) {
+          $.ajax({
+              url: '/scouts',
+              type: 'GET',
+              success: function (data) {
+                  $dfd.resolve({ "Result": "OK", "Records": data, "TotalRecordCount": data.length });
+              },
+              error: function () {
+                  $dfd.reject();
+              }
+          });
+        });
+      }
+    }
+  });
+  $('#ScoutTableContainer').jtable('load');
 });
