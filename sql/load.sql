89e418f2fed645b82fff61a89c3d1dfd88d43ac4
Richard Quintin <rquintin@vt.edu>
2018-08-30 14:48:13 -0400

    use sequence to generate ids

--- sql/load.sql
+++ sql/load.sql
@@ -1,14 +1,14 @@
 /* Sample data to be inserted at run time */
-insert into ScoutTrip (id, TripDate, Location, MinAge, MinRank) values (1, '2018-06-01', 'Grand Canyon', 14, 'Star');
-insert into ScoutTrip (id, TripDate, Location, MinAge, MinRank) values (2, '2018-05-09', 'Mountain Lake', null, null);
-insert into ScoutTrip (id, TripDate, Location, MinAge, MinRank) values (3, '2018-04-06', 'Camp Ottari', null, 'Tenderfoot');
-insert into ScoutTrip (id, TripDate, Location, MinAge, MinRank) values (4, '2018-03-12', 'New River', 12, null);
-insert into ScoutTrip (id, TripDate, Location, MinAge, MinRank) values (5, '2018-02-18', 'Claytor Lake', null, 'Scout');
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-06-01', 'Grand Canyon', 14, 'Star');
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-05-09', 'Mountain Lake', null, null);
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-04-06', 'Camp Ottari', null, 'Tenderfoot');
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-03-12', 'New River', 12, null);
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-02-18', 'Claytor Lake', null, 'Scout');
 
 
-insert into Scout (id, Name, DOB, Rank) values (1, 'Joe', '2006-03-21', 'Tenderfoot');
-insert into Scout (id, Name, DOB, Rank) values (2, 'Parikh', '2004-03-21', 'Star');
-insert into Scout (id, Name, DOB, Rank) values (3, 'Timmy', '2002-03-21', 'Eagle');
+insert into Scout (Name, DOB, Rank) values ('Joe', '2006-03-21', 'Tenderfoot');
+insert into Scout (Name, DOB, Rank) values ('Parikh', '2004-03-21', 'Star');
+insert into Scout (Name, DOB, Rank) values ('Timmy', '2002-03-21', 'Eagle');
 
 
 insert into attends (ScoutTrip_Id, Scout_Id) values (1, 3);
