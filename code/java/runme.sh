8c3783b461ac826c407e45037ddec2fd63ad7a4b
Richard Quintin <rquintin@vt.edu>
2018-09-07 15:24:05 -0400

    run app in background

--- code/java/runme.sh
+++ code/java/runme.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 docker build -t app -f ./Dockerfile ../.. && \
-docker run -it --rm -p 8080:8080 \
+docker rm -f app
+docker run --rm -p 8080:8080 \
   -v $(pwd)/maven:/maven \
   -v $(pwd)/troop:/code \
   --link postgres:postgres \
+  --name app \
   app $*
+
+
+cat <<EOF
+To view database logs issue command:
+
+```
+docker logs postgres
+```
+
+To view application logs issue command:
+
+```
+docker logs app
+```
+EOF
