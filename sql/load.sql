2ccd7f0cbf7846508db7bddf010a743cddff2139
Richard Quintin <rquintin@vt.edu>
2018-09-20 09:54:14 -0400

    update data

--- sql/load.sql
+++ sql/load.sql
@@ -1,20 +1,25 @@
-/* Sample data to be inserted at run time */
-insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-06-01', 'Grand Canyon', 14, 'Star');
-insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-05-09', 'Mountain Lake', null, null);
-insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-04-06', 'Camp Ottari', null, 'Tenderfoot');
-insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-03-12', 'New River', 12, null);
+/* Sample data to be inserted at run time 
+   Note we fudge the dates to provide meaningful examples in the interface.
+*/
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values (current_date, 'Grand Canyon', 14, 'Star');
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values (current_date - interval '8 days', 'Mountain Lake', null, null);
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values (current_date - interval '90 days', 'Camp Ottari', null, 'Tenderfoot');
+insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values (current_date - interval '7 months', 'New River', 12, null);
 insert into ScoutTrip (TripDate, Location, MinAge, MinRank) values ('2018-02-18', 'Claytor Lake', null, 'Scout');
 
 
 insert into Scout (Name, DOB, Rank) values ('Joe', '2006-03-21', 'Tenderfoot');
 insert into Scout (Name, DOB, Rank) values ('Parikh', '2004-03-21', 'Star');
 insert into Scout (Name, DOB, Rank) values ('Timmy', '2002-03-21', 'Eagle');
+insert into Scout (Name, DOB, Rank) values ('Sally', '2008-03-21', 'First Class');
 insert into Scout (Name, DOB, Rank) values ('Lazy Larry', '2006-03-21', 'Scout');
 
 
 
 insert into attends (ScoutTrip_Id, Scout_Id) values (1, 3);
 insert into attends (ScoutTrip_Id, Scout_Id) values (2, 1);
-insert into attends (ScoutTrip_Id, Scout_Id) values (2, 2);
-insert into attends (ScoutTrip_Id, Scout_Id) values (2, 3);
+insert into attends (ScoutTrip_Id, Scout_Id) values (3, 2);
+insert into attends (ScoutTrip_Id, Scout_Id) values (4, 4);
+
+
 
