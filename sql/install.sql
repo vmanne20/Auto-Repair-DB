b09932864fd2e558899adcdddf88661474ddf5b0
Vamsi Manne <vmanne20@Vamsis-MacBook-Pro.local>
2019-04-13 18:15:04 -0400

    change fields for PhoneNumber

--- sql/install.sql
+++ sql/install.sql
@@ -51,10 +51,10 @@ CREATE TABLE Customer
 
 CREATE TABLE PhoneNumber
 (
-  customer_id INT NOT NULL,
+  c_id VARCHAR(20) NOT NULL,
   c_name VARCHAR(60) NOT NULL,
   c_number CHAR(20) NOT NULL,
-  PRIMARY KEY (c_number),
+  PRIMARY KEY (c_number)
 --   FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
