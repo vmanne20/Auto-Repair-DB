82e64441fad23745c684fb081041189e89309d84
Vamsi Manne <vmanne20@Vamsis-MacBook-Pro.local>
2019-04-17 11:56:00 -0400

    list car id's

--- code/java/troop/static/js/cars.js
+++ code/java/troop/static/js/cars.js
@@ -6,7 +6,7 @@
             car_id: {
                 title: 'Car Id',
                 key: true,
-                list: false
+                list: true
             },
             c_id: {
                 title: 'Customer Id',
