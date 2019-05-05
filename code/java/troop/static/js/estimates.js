22886e8bf660d4e9ce04f50d2ecd9ae7c217877f
Vamsi Manne <vmanne20@Vamsis-MacBook-Pro.local>
2019-05-05 18:47:12 -0400

    test dynamic form creation

--- code/java/troop/static/js/estimates.js
+++ code/java/troop/static/js/estimates.js
@@ -2,21 +2,30 @@ $(document).ready(function () {
     let repairNames;
     return $.Deferred(function ($dfd) {
         $.ajax({
-          url: '/get-repair-names',
-          type: 'GET',
-          success: function (data) {
-            repairNames = data;
-            console.log("All Repair Names: ");
-            // console.log(repairNames);
-            for (let i = 0; i < repairNames.length; i++) {
-                console.log(repairNames[i]);
+            url: '/get-repair-names',
+            type: 'GET',
+            success: function (data) {
+                repairNames = data;
+                console.log("All Repair Names: ");
+                for (let i = 0; i < repairNames.length; i++) {
+                    console.log(repairNames[i]);
+                    let formInput = document.createElement("INPUT");
+                    formInput.setAttribute("type", "checkbox");
+                    formInput.setAttribute("name", "repair" + i);
+                    formInput.setAttribute("value", repairNames[i]);
+                    document.getElementById("estimate_form").appendChild(formInput);
+                }
+
+                let submitButton = document.createElement("INPUT");
+                submitButton.setAttribute("type", "submit");
+                submitButton.setAttribute("value", "Get Estimates");
+                document.getElementById("estimate_form").appendChild(submitButton);
+
+                $dfd.resolve({ "Result": "OK", "Records": data, "TotalRecordCount": data.length });
+            },
+            error: function () {
+                $dfd.reject();
             }
-            $dfd.resolve({ "Result": "OK", "Records": data, "TotalRecordCount": data.length });
-          },
-          error: function () {
-            $dfd.reject();
-          }
         });
     });
-
 });
